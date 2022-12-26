f5043564c5d71ef5f49d9c28a21838ff45835798
yundukhyun <kiml2175@nate.com>
2022-12-26 14:24:50 +0900

    Feat- 카카오톡 소셜로그인 추가

--- sansansgear/src/main/java/com/clone/sansansgear/entity/User.java
+++ sansansgear/src/main/java/com/clone/sansansgear/entity/User.java
@@ -1,6 +1,7 @@
 package com.clone.sansansgear.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,32 +9,46 @@ import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String userId;
+    private Long kakaoId; //(숫자일껍니다)
 
     @Column
-    private String password;
+    private String userId; //(email들어가야함)
 
     @Column
-    private String username;
+    private String password; //UUID로 랜덤하게 겹치지않게 넣어줌
 
     @Column
-    private String role;
+    private String username; // 닉네임 기입
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;  //권한 무조건 USER
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Bucket> bucketList = new ArrayList<>();
 
+    public User(String username, Long kakaoId, String password,String email, UserRoleEnum role) {
+        this.username = username;
+        this.kakaoId = kakaoId;
+        this.password = password;
+        userId = email;
+        this.role = role;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
+    }
 
 
-
-
-//    @Column
+    //    @Column
 //    private int phoneNumber;
 //
 //    @Column
@@ -41,3 +56,4 @@ public class User {
 
 
 }
+
