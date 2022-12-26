da3b983111ed171a72c457aa4ca3d2cec704a8b9
jihopark01 <jihop3355@gmail.com>
2022-12-27 01:58:10 +0900

    카테고리별 조회 기능 수정 완료

--- sansansgear/src/main/java/com/clone/sansansgear/controller/PostController.java
+++ sansansgear/src/main/java/com/clone/sansansgear/controller/PostController.java
@@ -23,30 +23,30 @@ public class PostController {
         return postService.uploadPost(postRequestDto);
     }
     //메인페이지상품조회
-    @GetMapping("/postList")
+    @GetMapping("/posts")
     public PostListResponseDto getPosts(){
         return postService.getPosts();
     }
     //제품상세조회
-    @GetMapping("/postList/{postId}")
+    @GetMapping("/posts/{postId}")
     public ResponseEntity<?> getPost(@PathVariable Long postId){
 
         return postService.getPost(postId);
     }
     //제품정보수정
-    @PutMapping("/postList/{postId}")
+    @PutMapping("/posts/{postId}")
     public ResponseEntity<?> updatePost(@PathVariable Long postId, @ModelAttribute PostRequestDto postRequestDto){
         return postService.updatePost(postId,postRequestDto);
     }
 
    //제품페이지삭제
-    @DeleteMapping("/postList/{postId}")
+    @DeleteMapping("/posts/{postId}")
     public ResponseDto deletePost(@PathVariable Long postId){
 
         return postService.deletePost(postId);
     }
     //카테고리별조회
-    @GetMapping("/postList/category")
+    @GetMapping("/posts/category")
     public CateResponseDto showCategory(@RequestParam("category") String category){
         return postService.showCate(category);
     }
